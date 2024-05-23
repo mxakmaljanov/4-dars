cf4daea5425224072c34284a3502bd6fe057a66e
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-05-24 00:27:33 +0500

    uy ishi

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+ //1chi masala
+ //let v = 23;
+ //let V = 1000;
+ //let G = (v+V)%24;
+ //let A = +prompt("a")
+ //console.log(g);
+//2-masala
 //let s=+prompt("selsiyni kiriting")
 //let k=s+273.15
 //let f=s*1.8+32
 //console.log("k")
 //console.log("f")
-//2-misol
+//3-misol
 //let t=10
 //let n=80
 //
@@ -11,10 +18,10 @@
 //if(t*b/n{
 //    console.log("chiqgan javob" )
 //}
-//3-misol
+//4-misol
 //let kb=30
 //let ichb=180
-//4-misol
+//5-misol
 //let s=15
 //let sb=50
 //let q=10
@@ -24,7 +31,7 @@
 //} else{
 //    console.log(ozgina bor) 
 //}
-//5-misol
+//6-misol
 //let v=+prompt("vaqtni kiriting")
 //let b=+prompt("vaqtni kiriting")
 // if(v+b){
