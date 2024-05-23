8d94f42af0c7dc4be98340868c37b127f084fdea
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-05-24 00:12:37 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,33 @@
+//let s=+prompt("selsiyni kiriting")
+//let k=s+273.15
+//let f=s*1.8+32
+//console.log("k")
+//console.log("f")
+//2-misol
+//let t=10
+//let n=80
+//
+//let b=60
+//if(t*b/n{
+//    console.log("chiqgan javob" )
+//}
+//3-misol
+//let kb=30
+//let ichb=180
+//4-misol
+//let s=15
+//let sb=50
+//let q=10
+//let k=20
+//if(s+q || s+k){
+//    console.log(yetting)
+//} else{
+//    console.log(ozgina bor) 
+//}
+//5-misol
+//let v=+prompt("vaqtni kiriting")
+//let b=+prompt("vaqtni kiriting")
+// if(v+b){
+//    console.log(vaqt shu)
+// }
+
